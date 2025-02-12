94046877e892b8727d739bb023f6530e18c92b97
UmbraSun <kirill.xodyukov@gmail.com>
2025-02-12 16:11:03 +0600

    get query

--- BLL/Features/TestContext/Queries/TestGetQuery.cs
+++ BLL/Features/TestContext/Queries/TestGetQuery.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace BLL.Features.TestContext.Queries
 {
+    /// <summary>
+    /// Get query
+    /// </summary>
     public class TestGetQuery : IRequest<int>
     {
-
     }
 
+    public class TestGetQueryHandler : IRequestHandler<TestGetQuery, int>
+    {
+        private readonly ILogger<TestGetQueryHandler> _logger;
 
+        public TestGetQueryHandler(ILogger<TestGetQueryHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(TestGetQuery request, CancellationToken cancellationToken)
+        {
+            return 42;
+        }
+    }
 }
